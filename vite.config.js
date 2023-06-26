443a49e48926dc2a1c0d88859067e4ea9879915d
Anna Rybkina <anna.rybkina@anicode.se>
2023-06-26 10:21:25 +0200

    fix: remove docs and update deps

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import mdx from 'vite-plugin-mdx';
 import react from '@vitejs/plugin-react';
-import slug from 'remark-slug';
-import autoLinkHeadings from 'remark-autolink-headings';
-import jsxExample from './docs/src/plugins/jsxExample.cjs';
-import propTable from './docs/src/plugins/propTable.cjs';
 import { presetWarp } from '@warp-ds/uno'
 import uno from 'unocss/vite'
-import { classes } from '@warp-ds/component-classes/classes';
 
 
 export default function config() {
@@ -15,12 +9,8 @@ export default function config() {
     plugins: [
       uno({
         presets: [presetWarp({ usePreflight: true })],
-        safelist: classes,
       }),
       react(),
-      mdx.default({
-        remarkPlugins: [slug, autoLinkHeadings, jsxExample, propTable],
-      }),
     ],
     build: {
       outDir: 'site',
