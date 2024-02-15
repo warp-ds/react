67416f7318e84a0d683bea54b8ccc2efa0d0327e
imprashast <imprashast@gmail.com>
2024-02-15 13:36:23 +0100

    Generate types programatically

--- build.mjs
+++ build.mjs
@@ -1,5 +1,7 @@
 import esbuild from "esbuild";
 import { glob } from "glob";
+import fs from 'fs';
+import ts from 'typescript';
 
 const components = glob.sync("packages/**/src/index.ts");
 const indexPath = "packages/index.ts";
@@ -13,15 +15,55 @@ const esbuildDefaults = {
   external: ["react", "react-dom", "@lingui/core"],
 };
 
+function generateTypeDefinitions(inputFilePath, packageName, outDir) {
+  let listOfTsFiles = [];
+  const options = {
+    module: ts.ModuleKind.NodeNext,
+    lib: ["DOM", "ES2020"],
+    target: ts.ScriptTarget.ES2020,
+    sourceMap: true,
+    strict: true,
+    noImplicitReturns: true,
+    noFallthroughCasesInSwitch: true,
+    noUnusedLocals: true,
+    noUnusedParameters: true,
+    jsx: ts.JsxEmit.React,
+    allowJs: true,
+    declaration: true,
+    emitDeclarationOnly: true,
+    esModuleInterop: true,
+    skipLibCheck: true,
+    allowSyntheticDefaultImports: true,
+    strictNullChecks: true,
+    outDir: outDir,
+  }
+  const host = ts.createCompilerHost(options);
+  host.writeFile = (fileName, contents) => listOfTsFiles.push({ fileName, contents });
+
+  // Create a program using the parsed source file
+  const program = ts.createProgram([inputFilePath], options, host);
+
+  // Emit the generated type definitions in memory
+  program.emit();
+
+  // Write the generated type definitions to the output file
+  listOfTsFiles.forEach((file) => {
+    // Doing this hack since typescript sometimes doesn't output the correct path
+    let packageTypePath = file.fileName.replace(outDir, '').replace('src/', '').replace(`${packageName}/`, '');
+    const updatedFilename = outDir + packageTypePath;
+    
+    fs.mkdirSync(updatedFilename.split('/').slice(0, -1).join('/'), { recursive: true });
+    fs.writeFileSync(updatedFilename, file.contents);
+  });
+}
+
 function buildComponents(outDir, extraBuildOptions = {}) {
   components.forEach(async (item) => {
     const regex = /\/(\w+)\//;
     const match = item.match(regex);
 
     if (item.includes("utils")) return;
-    //console.log(`react: building ${match[1]}.js`);
-
-    console.log(`"./components/${match[1]}": "./dist/npm/packages/${match[1]}/index.js",`);
+    console.log(`react: building ${match[1]}.js`);
 
     try {
       await esbuild.build({
@@ -30,6 +72,7 @@ function buildComponents(outDir, extraBuildOptions = {}) {
         ...esbuildDefaults,
         ...extraBuildOptions,
       });
+      generateTypeDefinitions(item, match[1],`${outDir}/packages/${match[1]}/`);
     } catch (err) {
       console.error(err);
     }
