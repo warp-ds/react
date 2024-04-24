bc69c47e07b99cf22257907bbde34cab4e529d2d
imprashast <imprashast@gmail.com>
2024-04-24 10:57:53 +0200

    Add some tests

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,6 @@ export default function config(env) {
       exclude: ['**.json'],
       coverage: {
         cleanOnRerun: true,
-        skipFull: true,
         reporter: ['text'],
         exclude: [
           "**.json",
@@ -33,7 +32,9 @@ export default function config(env) {
           "esbuild.mjs",
           "lingui.config.ts",
           "@types",
-          "tests/"
+          "tests/",
+          "packages/**/src/props.*",
+          "packages/**/src/index.*"
         ],
       },
     },
