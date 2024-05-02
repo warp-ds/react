e9c7d2541c701036c221c1965d1dac513fdf7709
felicia-haggqvist <felicia.haggqvist@schibsted.com>
2024-05-02 11:10:27 +0200

    feat: add  ts,tsx,jsx files in lint:check and lint

--- packages/utils/src/useIsomorphicLayoutEffect.ts
+++ packages/utils/src/useIsomorphicLayoutEffect.ts
@@ -1,5 +1,4 @@
 import { useLayoutEffect as useLayoutEffectSafely, useEffect } from 'react';
 
 // Ensure the name used in components is useLayoutEffect so the eslint react hooks plugin works
-export const useLayoutEffect =
-  typeof window !== 'undefined' ? useLayoutEffectSafely : useEffect;
+export const useLayoutEffect = typeof window !== 'undefined' ? useLayoutEffectSafely : useEffect;
