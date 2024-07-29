a304d5898bca11165898075e8dc3d9e1ff3ba5a5
felicia-haggqvist <felicia.haggqvist@schibsted.com>
2024-07-29 16:01:13 +0200

    fix(card): replace nested ternary expressions with an if-statement for better readability

--- packages/card/src/component.tsx
+++ packages/card/src/component.tsx
@@ -6,17 +6,20 @@ import { card as ccCard } from '@warp-ds/css/component-classes';
 import { CardProps } from './props.js';
 
 export function Card(props: CardProps) {
-  const { as = 'div', children, flat, ...rest } = props;
+  const { as = 'div', children, flat, selected, ...rest } = props;
+
+  let backgroundClass: string;
+  if (selected) {
+    backgroundClass = flat ? ccCard.cardFlatSelected : ccCard.cardSelected;
+  } else {
+    backgroundClass = flat ? ccCard.cardFlatUnselected : ccCard.cardShadowBackground;
+  }
 
   return React.createElement(
     as,
     {
       ...rest,
-      className: classNames(props.className, [
-        ccCard.card,
-        props.flat ? ccCard.cardFlat : ccCard.cardShadow,
-        props.selected ? (props.flat ? ccCard.cardFlatSelected : ccCard.cardSelected) : props.flat && ccCard.cardFlatUnselected,
-      ]),
+      className: classNames(props.className, [ccCard.card, props.flat ? ccCard.cardFlat : ccCard.cardShadow, backgroundClass]),
     },
     <>
       {!props.flat && (
