d4c783c962a57bdcdfafb75c843d2ad71e2185e7
adam-zawisza-jalaluddin-ahmed <adam.zawisza.jalaluddin.ahmed@schibsted.com>
2025-01-08 11:25:37 +0100

    cleanup, minor changes

--- packages/slider/src/component.tsx
+++ packages/slider/src/component.tsx
@@ -1,147 +1,9 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { classNames } from "@chbphone55/classnames";
-import { createHandlers, useDimensions } from "@warp-ds/core/slider";
 import { slider as ccSlider } from "@warp-ds/css/component-classes";
 
 import { SliderProps } from "./props.js";
-
-/* const keys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "End", "Home", "PageUp", "PageDown"]  as const;
-type Key = typeof keys[number]; */
-
-/*export function Slider({ min = 0, max = 100, step = 1, value: initialValue, disabled, onChange, onChangeAfter, ...rest }: SliderProps) {
-  const sliderLine = useRef<HTMLDivElement | null>(null);
-  const thumbRef = useRef<HTMLDivElement | null>(null);
-
-  const [value, setValue] = useState(initialValue);
-  const [finalValue, setFinalValue] = useState(initialValue);
-  const [position, setPosition] = useState(initialValue);
-  const [dimensions, setDimensions] = useState({ left: 0, width: 0 });
-  const [sliderPressed, setSliderPressed] = useState(false);
-
-  const sliderState = useMemo(
-    () => ({
-      get position() {
-        return position;
-      },
-      set position(v) {
-        setPosition(v);
-      },
-      get sliderPressed() {
-        return sliderPressed;
-      },
-      set sliderPressed(v) {
-        setSliderPressed(v);
-      },
-      get val() {
-        return value;
-      },
-      set val(v) {
-        setValue(v);
-      },
-      get thumbEl() {
-        return thumbRef.current;
-      },
-      get dimensions() {
-        return dimensions;
-      },
-      get step() {
-        return step;
-      },
-    }),
-    [position, sliderPressed, value, dimensions, step],
-  );
-
-  const { handleKeyDown, handleFocus, handleBlur, handleMouseDown, handleClick, getThumbPosition, getThumbTransform, getShiftedChange } =
-    createHandlers({ props: { min, max, step, ...rest }, sliderState });
-
-  const { mountedHook, unmountedHook } = useDimensions();
-
-  useEffect(() => {
-    if (!sliderLine.current) return;
-    mountedHook(sliderLine.current, setDimensions);
-    return () => unmountedHook();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [sliderLine]);
-
-  useEffect(() => {
-    if (value !== initialValue) onChange?.(value);
-  }, [value, initialValue, onChange]);
-
-  useEffect(() => {
-    if (!sliderPressed && value !== finalValue) {
-      setFinalValue(value);
-      onChangeAfter?.(value);
-    }
-  }, [onChangeAfter, sliderPressed, value, finalValue]);
-
-  useEffect(() => {
-    // prevents shiftedChange when modelValue was set externally
-    if (position !== initialValue) {
-      const nextVal = step ? getShiftedChange(position) : position;
-      if (nextVal !== value) {
-        setValue(nextVal);
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [position, initialValue, step]);
-
-  useEffect(() => {
-    if (!sliderPressed && position !== initialValue && value !== initialValue) {
-      setPosition(initialValue);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [sliderPressed, initialValue]);
-
-  const thumbPosition = useMemo(getThumbPosition, [getThumbPosition]);
-  const sliderActiveStyle = useMemo(
-    () => ({
-      left: 0,
-      right: `${100 - thumbPosition}%`,
-    }),
-    [thumbPosition],
-  );
-
-  const transformValue = useMemo(getThumbTransform, [getThumbTransform]);
-  const thumbStyles = useMemo(
-    () => ({
-      transform: `translateX(${transformValue}px)`,
-    }),
-    [transformValue],
-  );
-
-  const trackClasses = classNames([ccSlider.track, disabled && ccSlider.trackDisabled]);
-
-  const activeTrackClasses = classNames([ccSlider.activeTrack, disabled ? ccSlider.activeTrackDisabled : ccSlider.activeTrackEnabled]);
-
-  const thumbClasses = classNames([ccSlider.thumb, disabled ? ccSlider.thumbDisabled : ccSlider.thumbEnabled]);
-
-  return (
-    <div className={ccSlider.wrapper}>
-      <div ref={sliderLine} className={trackClasses} onClick={handleClick} />
-      <div className={activeTrackClasses} style={sliderActiveStyle} onClick={handleClick} />
-      <div
-        role="slider"
-        tabIndex={0}
-        className={thumbClasses}
-        ref={thumbRef}
-        style={thumbStyles}
-        aria-label={rest['aria-label']}
-        aria-labelledby={rest['aria-labelledby']}
-        aria-valuemin={min}
-        aria-valuemax={max}
-        aria-valuenow={value}
-        aria-valuetext={rest['aria-valuetext']}
-        onMouseDown={(e) => handleMouseDown(e as unknown as KeyboardEvent)}
-        onTouchStart={(e) => handleMouseDown(e as unknown as KeyboardEvent)}
-        onBlur={handleBlur}
-        onFocus={handleFocus}
-        onKeyDown={(e) => handleKeyDown(e as unknown as KeyboardEvent)}
-      />
-    </div>
-  );
-}
-*/
 
 /* 
 New slider component, capable of being used as either a standard slider (one value) or a range slider (using an array of two values).
@@ -288,7 +150,7 @@ export function Slider({
   };
 
   // Use the given value to get full value array.
-  const getValues = (value: number, index = 0) => {
+  const getValues = (value: number, index = 1) => {
     let values: number[];
 
     value = Math.round(value);
@@ -340,7 +202,7 @@ export function Slider({
 
     const width = wrapperRef.current?.clientWidth || 500;
 
-    const left = width * (currentValues[0] / max);
+    const left = isRange ? width * (currentValues[0] / max) : 0;
 
     return {
       width: widthFraction * 100 + "%",
@@ -423,7 +285,7 @@ export function Slider({
           onMouseOut={onInputComplete}
         >
           {inputElement(1)}
-          {isRange ? inputElement(0) : undefined}
+          {isRange && inputElement(0)}
         </div>
       </div>
     </>
